1032e03af9180cac33124d88eb127dfab9a313aa
Franz <franz.fuchs97@gmx.de>
2018-09-24 23:05:17 +0200

    added the report files and did some first implementation steps

--- TTT/player.cpp
+++ TTT/player.cpp
@@ -4,6 +4,41 @@
 namespace TICTACTOE
 {
 
+/**
+ * evaluates a certain state
+ * returns a value that says how
+ * good a state is for a player
+ */
+int evaluate(const GameState &pState)
+{
+    return -1;
+}
+/**
+ * implementation of the alpa-beta
+ * pruning algorithm adapted to the
+ * 2D TIC-TAC-TOE game
+ */
+int minimax(const GameState &pState)
+{
+    std::cout << "Call of method minimax\n";
+
+    std::vector<GameState> lNextStates;
+    pState.findPossibleMoves(lNextStates);
+
+    int maxIndex = -1;
+    int maxValue = -1000;
+    for(int i = 0; i < lNextStates.size(); i++)
+    {
+        int value = minimax(lNextStates[i]);
+        if(value > maxValue){
+            value = maxValue;
+            maxIndex = i;
+        }
+    }
+
+    return maxIndex;
+}
+
 GameState Player::play(const GameState &pState,const Deadline &pDue)
 {
     //std::cerr << "Processing " << pState.toMessage() << std::endl;
@@ -11,6 +46,11 @@ GameState Player::play(const GameState &pState,const Deadline &pDue)
     std::vector<GameState> lNextStates;
     pState.findPossibleMoves(lNextStates);
 
+
+
+    // we are already in a terminal state
+    // so we cannot do anything else than
+    // returning what we got
 
     if (lNextStates.size() == 0) return GameState(pState, Move());
 
